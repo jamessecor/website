999dcebf2ee815336d6ca0eb7a079ce3ca34d29d
James Secor <james.secor@gmail.com>
2017-02-09 06:53:28 -0500

    Form Validated

--- contact.php
+++ contact.php
@@ -1,16 +1,62 @@
 <?PHP
 include "nomoHeader.php";
-?>
+print "<script type='text/javascript' src='./contactScript.js'></script>";
+$validation="failed";
+$name="";
+$email="";
+$memo="";
+$errors=array();
 
-<form id='contactForm' action='process_form.php' method='post'>
-Name <input type='text' name='fullname' value=" <?php echo isset($_POST['fullname']) ? $_POST['fullname']: ""; ?>"  required><br>
-Email* <input type='email' name='email' required><br>
-Message <textarea name='comment' placeholder='Enter comments here.'></textarea><br>
-<input type='submit' name='submit' value='Submit'>
+if(isset($_POST['submit'])) {
+	// Name
+	if(!empty($_POST['name'])) {
+		$name=$_POST['name'];
+		if(strlen(trim($name))==0)
+			$errors['name']="Name cannot be blank.";
+		else if(!preg_match("/^[a-zA-Z '-.]+$/", $name))
+			$errors['name']="Name cannot include special characters.";
+	} else {
+		$errors['name']="This field is required.";
+	}
+	
+	// Email
+	if(!empty($_POST['email'])) {
+		$email=$_POST['email'];
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			$errors['email']="Enter a valid email address.";
+		}
+	} else {
+		$errors['email']="This field is required.";
+	}
+	
+	// Show errors
+	$errorCount=count($errors);
+	if($errorCount==0)
+		$validation="Success";		
+	else
+		print "<small class='errorText'>There are $errorCount errors. <br>Please make corrections and try again</small>";		
+}
+
+if($validation=="Success") {
+	print "<script>turnOn();</script>";
+	$len=strlen($name);
+	print "<div id='formResponse'>";
+	print "<p>Success!<br><br>Thank you,<br>$name.</p>";
+	print "<p>Your information <br>has been saved.</p>";
+	print "</div>";
+} else {
+
+?>
+<form id='contactForm' action="" method='post'>
+Name <input type='text' name='name' value="<?php echo isset($_POST['name']) ? $_POST['name']: ''; ?>"  required="required"><br>
+Email* <input type='email' name='email' value="<?php echo isset($_POST['email']) ? $_POST['email'] : ''; ?>"required="required"><br>
+Message <textarea name='memo' placeholder='Enter comments here.' ><?php echo isset($_POST['memo']) ? $_POST['memo']: '' ?></textarea><br>
+<input type='submit' name='submit' value='Submit' formnovalidate>
 <p id='disclaimer'>*Only the artist will see your email</p>
 </form>
 
-<script type="text/javascript" src="./contactScript.js"></script>
+
 <?PHP
+}
 include "nomoFooter.php"
 ?>
--- contactScript.js
+++ contactScript.js
@@ -2,7 +2,7 @@ function onLoading() {
 	// Phone Screen (canvas element)
 	var c = document.getElementById("phone");
 	var ctx = c.getContext("2d");
-	ctx.fillStyle = '#eaa';
+	ctx.fillStyle = '#ecc';
 	ctx.clearRect(0,0,phone.width,phone.height);
 	ctx.fillRect(0,0,phone.width,phone.height);
 	
