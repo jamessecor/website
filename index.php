8e980e96f70dac58b62cdcd7a0bd35a7258e1600
James Secor <james.secor@gmail.com>
2017-02-19 16:42:34 -0500

    Changed images and info text

--- index.php
+++ index.php
@@ -3,12 +3,10 @@ include "nomoHeader.php";
 ?>
 
 <!-- Images -->
-<img class="nomoImages" style="display:none" src="C:\Users\James\Dropbox\nomophobiaImages\1.jpg" alt="no image available">
-<img class="nomoImages" style="display:none" src="C:/users/james/dropbox/nomophobiaImages/2.jpg" alt="no image available">
-<img class="nomoImages" style="display:none" src="C:\Users\James\Dropbox\nomophobiaImages\3.jpg" alt="no image available">
-<img class="nomoImages" style="display:none" src="C:/users/james/dropbox/nomophobiaImages/4.jpg" alt="no image available">
-<img class="nomoImages" style="display:none" src="C:\Users\James\Dropbox\nomophobiaImages\5.jpg" alt="no image available">
-<img class="nomoImages" style="display:none" src="C:/users/james/dropbox/nomophobiaImages/6.jpg" alt="no image available">
+<img class="nomoImages" style="display:none" src="https://dl.dropboxusercontent.com/u/33729311/nomophobiaImages/01.jpg" alt="no image available">
+<img class="nomoImages" style="display:none" src="https://dl.dropboxusercontent.com/u/33729311/nomophobiaImages/02.jpg" alt="no image available">
+<img class="nomoImages" style="display:none" src="https://dl.dropboxusercontent.com/u/33729311/nomophobiaImages/03.jpg" alt="no image available">
+<img class="nomoImages" style="display:none" src="https://dl.dropboxusercontent.com/u/33729311/nomophobiaImages/04.jpg" alt="no image available">
 
 <!-- Apps -->
 <button id="contact" class="app" style="display:none">join email list</button>
@@ -20,13 +18,13 @@ include "nomoHeader.php";
 <button id="info" class="app" style="display:none">#nomo- phobia info</button>
 
 <button id="showImages" class="app" style="display:none">images</button>
-<!-- Next Image -->
+<!-- Next Images -->
 <button id="next" class="imgButtons" style="display:none">next</button>
 
 <!-- Info Text -->
 <div id="infoText" style="display:none">
-<p>#nomophobia<br><a href="jamessecor.weebly.com">James Secor</a><br><br>See it at <br><a href="http://www.studioplacearts.com">Studio Place Arts</a><br>201 N Main St<br>Barre, VT 05641
-<br><br>March 14-April 15, 2017</p>
+<p><strong>#nomophobia</strong><br><a href="jamessecor.weebly.com">James Secor</a><br><br>See it at <br><a href="http://www.studioplacearts.com">Studio Place Arts</a><br>201 N Main St<br>Barre, VT 05641
+<br><br>March 14-April 15, 2017</p><p><small>Opening</small> March 18, 4-6pm</p>
 </div>
 
 <script type="text/javascript" src="./volumeScript.js"></script>
